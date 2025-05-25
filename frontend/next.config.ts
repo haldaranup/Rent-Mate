daa42d78b1ff62fc1ffb2e1aa2f43f2e67252540
Anup Haldar <haldaranup746@gmail.com>
2025-05-25 10:57:11 +0530

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/auth/:path*', // Proxy requests starting with /auth/
-        destination: 'http://localhost:8080/', // To your backend
+        destination: process.env.NEXT_PUBLIC_API_BASE_URL || 'localhost:8080/', // To your backend
       },
       // You can add more rewrite rules here for other backend services if needed
     ];
